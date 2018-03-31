d154e3acd64afe123cbaa1a4986ebab25757fa4f
victorcheng7 <victorcheng@Victors-MacBook-Pro.local>
2018-03-31 03:19:15 -0700

    first commit

---
+++ contracts/Casino.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.4.20;
+
+contract Casino {
+   address public owner;
+   uint256 public minimumBet;
+   uint256 public totalBet;
+   uint256 public numberOfBets;
+   uint256 public maxAmountOfBets = 100;
+   address[] public players;
+
+   struct Player {
+      uint256 amountBet;
+      uint256 numberSelected;
+   }
+   // The address of the player => user info
+   mapping(address => Player) public playerInfo;
+
+   function Casino(uint256 _minimumBet) public {
+      owner = msg.sender;
+      if(_minimumBet != 0) minimumBet = _minimumBet;
+   }
+   function kill() public {
+      if(msg.sender == owner) selfdestruct(owner);
+   }
+
+   // Bet for a number between 1 and 10 both inclusive
+   function bet(uint256 numberSelected) public payable {
+      require(!checkPlayerExists(msg.sender));
+      require(numberSelected >= 1 && numberSelected <= 10);
+      require(msg.value >= minimumBet);
+      playerInfo[msg.sender].amountBet = msg.value;
+      playerInfo[msg.sender].numberSelected = numberSelected;
+      numberOfBets++;
+      players.push(msg.sender);
+      totalBet += msg.value;
+
+      if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
+   }
+
+   function checkPlayerExists(address player) public constant returns(bool){
+      for(uint256 i = 0; i < players.length; i++){
+          if(players[i] == player) return true;
+      }
+      return false;
+   }
+
+   // Generates a number between 1 and 10 that will be the winner
+   function generateNumberWinner() public {
+      uint256 numberGenerated = block.number % 10 + 1; // TODO change to be random
+      distributePrizes(numberGenerated);
+   }
+}
