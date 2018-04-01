4372e52f6d7b65bcfe15bed712c98bd2f2a2fb04
victorcheng7 <victorcheng@Victors-MacBook-Pro.local>
2018-03-31 17:31:57 -0700

    finished contract

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
       filename: 'build.js' // The final file will be created in dist/build.js
    },
    module: {
-      loaders: [{
+      rules: [{
          test: /\.css$/, // To load the css in react
          use: ['style-loader', 'css-loader'],
          include: /src/
@@ -19,7 +19,8 @@ module.exports = {
          }
       }, {
          test: /\.json$/, // To load the json files
-         loader: 'json-loader'
+         loader: 'json-loader',
+         exclude: /node_modules/
       }]
    }
 }
